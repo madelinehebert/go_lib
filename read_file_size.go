2e7a0efa8d5b39f07ad8099bcf248980419ce4e2
maddie <madelinehebert@proton.me>
2024-04-13 12:32:40 -0400

    Added ReadFileSize.go

---
+++ read_file_size.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func ReadFileSize(filepath string) int {
+	//Read in file's size
+	/* Open a file for read access */
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/* find the file on the system, find the amount of bytes it occupies */
+	file_stats, err := file.Stat()
+	if err != nil {
+		log.Println(err)
+	}
+	file.Close()
+
+	return int(file_stats.Size())
+}
