d54b2f1871ce2509dd2a873bc16f3795a2c6987e
Maddie <maddie@laptop.localdomain>
2024-05-11 14:31:40 -0400

    Added file to get the current's machine's IP

---
+++ get_local_ip.go
@@ -0,0 +1,20 @@
+package
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+// Get preferred outbound ip of this machine
+func GetOutboundIP() string {
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+    return fmt.Sprint(localAddr.IP)
+}
