d7a1384b52b89f4fe0da90f64e23b736391dab0d
madelinehebert <madelinehebert@proton.me>
2024-05-11 14:49:05 -0400

    fixed function name in GetLocalIP()

--- get_local_ip.go
+++ get_local_ip.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Get preferred outbound ip of this machine
-func get_local_ip() string {
+func GetLocalIP() string {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
 		log.Fatal(err)
