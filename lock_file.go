b921f24f7e7a0e5ec598b9eff863af3fc8a05950
madelinehebert <madelinehebert@proton.me>
2024-05-15 14:18:49 -0400

    added back lock_file.go

---
+++ lock_file.go
@@ -0,0 +1,25 @@
+package go_lib
+
+import (
+	"os"
+	"syscall"
+	"time"
+
+	"github.com/madelinehebert/go_lib"
+)
+
+func lock_file(file *os.File) error {
+	//Set a lock
+	for {
+		//Attempt to set a lock on the file; if this fails, wait for a random amount of
+		//time between 0 and one seconds before retrying
+		if err := syscall.Flock((int(file.Fd())), syscall.LOCK_EX); err != nil {
+			time.Sleep(time.Duration(go_lib.GenerateRandomTime(0, 1)))
+		} else {
+			break
+		}
+	}
+
+	//If all is well, return nil, signalling the file was locked
+	return nil
+}
