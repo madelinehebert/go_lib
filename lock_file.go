5af4a2696fb53a2ebf818599584d9844bd6a8bf3
madelinehebert <madelinehebert@proton.me>
2024-05-15 14:19:56 -0400

    fixed import cycle

--- lock_file.go
+++ lock_file.go
@@ -4,8 +4,6 @@ import (
 	"os"
 	"syscall"
 	"time"
-
-	"github.com/madelinehebert/go_lib"
 )
 
 func LockFile(file *os.File) error {
@@ -14,7 +12,7 @@ func LockFile(file *os.File) error {
 		//Attempt to set a lock on the file; if this fails, wait for a random amount of
 		//time between 0 and one seconds before retrying
 		if err := syscall.Flock((int(file.Fd())), syscall.LOCK_EX); err != nil {
-			time.Sleep(time.Duration(go_lib.GenerateRandomTime(0, 1)))
+			time.Sleep(time.Duration(GenerateRandomTime(0, 1)))
 		} else {
 			break
 		}
