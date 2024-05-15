be411149a1d15aaa6f626311f921565d9f417dfc
madelinehebert <madelinehebert@proton.me>
2024-05-15 14:06:58 -0400

    fixed functioning naming conventions on generate_random_time.go

--- generate_random_time.go
+++ generate_random_time.go
@@ -2,7 +2,7 @@ package go_lib
 
 import "math/rand/v2"
 
-func generate_random_time(min int, max int) int {
+func GenerateRandomTime(min int, max int) int {
 	//Return the time
 	return rand.IntN(max-min) + min
 }
