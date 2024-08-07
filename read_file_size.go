5646a02555c32c7cfa59c89724e888801344ff04
madelinehebert <160977018+madelinehebert@users.noreply.github.com>
2024-08-06 23:21:07 -0400

    Added more comments to read_file_size.go

--- read_file_size.go
+++ read_file_size.go
@@ -5,19 +5,34 @@ import (
 	"os"
 )
 
+/* 
+
+   Read and return the file size of a file located at a provided file path.
+   
+   Negative return values are errors.
+   -1 is an inability to open a file for reading.
+   -2 is an inability to call os.Stat() on a file
+*/
+
+
 func ReadFileSize(filepath string) int {
-	//Read in file's size
+	/* Read in file's size */
 
 	/* Open a file for read access */
 	if file, err := os.Open(filepath); err != nil {
 		log.Println(err)
 		return -1
 	} else {
+		
+		/* Defer file closing */
 		defer file.Close()
+		
+		/* Call os.Stat() on file in order to retrieve file size. */
 		if file_stats, err := file.Stat(); err != nil {
                 	log.Println(err)
 			return -1
         	} else {
+			/* Allow defer to handle file closing and return file size to calling function. */
 			return int(file_stats.Size())
 		}
 
